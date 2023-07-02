713e1d1448230fa72323ed2dec90744287aed463
yuliahlushko <yellow.241190@gmail.com>
2023-07-03 00:11:20 +0300

    Homework3

--- Homework3.py
+++ Homework3.py
@@ -1,24 +1,25 @@
 action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
 notes = []
-while action in ['add']:
-    note = input('Додай нотатку: ')
-    notes.append(note)
-    print(notes)
-    action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-    continue
-if action == 'longest':
-    notes.sort(reverse=True, key=len)
-    print(notes)
-    action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-if action == 'shortest':
-    notes.sort(key=len)
-    print(notes)
-    action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-if action == 'earliest':
-    print(notes.reverse())
-    action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-if action == 'latest':
-    print(notes)
-    action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-if action == 'Exit':
-    print('Finish')
+while action in ['add', 'earliest', 'latest', 'longest', 'shortest', 'Exit']:
+        if action == 'add':
+            note = input('Додай нотатку: ')
+            notes.append(note)
+            print(notes)
+            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
+        if action == 'earliest':
+            print(notes)
+            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
+        if action == 'latest':
+            print(list(reversed(notes)))
+            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
+        if action == 'longest':
+            notes.sort(reverse=True, key=len)
+            print(notes)
+            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
+        if action == 'shortest':
+            notes.sort(key=len)
+            print(notes)
+            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
+        if action == 'Exit':
+            print('Finish')
+            break
