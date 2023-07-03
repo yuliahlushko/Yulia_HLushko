f56078d85700384b9e8b9edbbe5f3e8b38d92893
yuliahlushko <86711189+yuliahlushko@users.noreply.github.com>
2023-07-03 10:11:23 +0300

    Update Homework_3.py

--- Homework_3.py
+++ Homework_3.py
@@ -1,25 +1,20 @@
-action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
 notes = []
-while action in ['add', 'earliest', 'latest', 'longest', 'shortest', 'Exit']:
-        if action == 'add':
-            note = input('Додай нотатку: ')
-            notes.append(note)
-            print(notes)
-            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-        if action == 'earliest':
-            print(notes)
-            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-        if action == 'latest':
-            print(list(reversed(notes)))
-            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-        if action == 'longest':
-            notes.sort(reverse=True, key=len)
-            print(notes)
-            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-        if action == 'shortest':
-            notes.sort(key=len)
-            print(notes)
-            action = input('Оберіть дію (add, earliest, latest, longest, shortest, Exit): ')
-        if action == 'Exit':
-            print('Finish')
-            break
+i = True
+while i:
+    action = input('Оберіть дію (add, earliest, latest, longest, shortest, exit): ')
+    if action == 'add':
+        note = input('Додай нотатку: ')
+        notes.append(note)
+        print(notes)
+    elif action == 'earliest':
+        print(notes)
+    elif action == 'latest':
+        print(list(reversed(notes)))
+    elif action == 'longest':
+        notes.sort(reverse=True, key=len)
+        print(notes)
+    elif action == 'shortest':
+        notes.sort(key=len)
+        print(notes)
+    elif action == 'exit':
+        i = False
